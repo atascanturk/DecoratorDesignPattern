ccf0645d036810174e94051fe00adb66b2b9c006
atascanturk <atascanturk@gmail.com>
2021-05-31 21:10:17 +0300

    Add project files.

---
+++ DecoratorApp.UI/Decorator.Lib/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Lib.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T model);
+        void Update(T model);
+        void Delete(T model);
+    }
+}
---
+++ DecoratorApp.UI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Decorator.Entities.Concrete;
+using Decorator.Lib.Concrete;
+using Decorator.Lib.Decorator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorApp.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Repository");
+            Repository<User> repository = new Repository<User> ();
+            repository.GetById(1);
+            repository.GetAll();
+            repository.Add(new User());
+            repository.Delete(new User());
+            repository.Update(new User());
+
+            Console.WriteLine("\n****************");
+
+            Console.WriteLine("\nLoggingRepositoryDecorator");
+          
+            LoggingRepositoryDecorator<User> loggingRepositoryDecorator = new LoggingRepositoryDecorator<User>(repository);
+            loggingRepositoryDecorator.GetById(1);
+            loggingRepositoryDecorator.GetAll();
+            loggingRepositoryDecorator.Add(new User());
+            loggingRepositoryDecorator.Delete(new User());
+            loggingRepositoryDecorator.Update(new User());
+
+            Console.ReadLine();
+        }
+    }
+}
